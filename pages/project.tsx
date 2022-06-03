e0e7e0cf947f0b3732db58f0d685e8140d416bcc
sovla <sovla@naver.com>
2022-06-03 17:56:20 +0900

    - 애니메이션 기능 구현중

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from "react";
-import styled from "styled-components";
+import { useCallback, useMemo, useState } from "react";
+import styled, { css } from "styled-components";
 
 const Main = styled.div`
   width: 100vw;
@@ -16,16 +16,70 @@ const Box = styled.div`
   }
 `;
 
-const TagItem = styled.div`
+const TagItem = styled.div<{ isSelect: boolean }>`
+  user-select: none;
+  position: relative;
   border: 1px solid #222;
   background-color: #313131;
   border-width: 1px 1px 2px 1px;
   box-shadow: 1px 1px 0 rgb(0 0 0 / 25%);
   padding: 5px 7px;
   margin-right: 5px;
+  border-radius: 3px;
   & > p {
     color: #999;
     font-family: MontSerrat, Tahoma, Arial, sans-serif;
+  }
+  &::before {
+    content: "";
+    top: 0;
+    left: 0;
+    height: 50%;
+    display: block;
+    position: absolute;
+    z-index: 1;
+    border-radius: 3px 3px 0px 0px;
+    width: 100%;
+    ${(p) =>
+      !p.isSelect &&
+      css`
+        transform: scale(0);
+      `}
+    background-color: ${(p) => (p.isSelect ? "#9997" : "#0000")};
+    transition: transform 0.3s cubic-bezier(0.65, 0.05, 0.1, 1);
+  }
+  &::after {
+    content: "";
+    border-radius: 0px 0px 3px 3px;
+    bottom: 0;
+    left: 0;
+    display: block;
+    height: 50%;
+    position: absolute;
+    width: 100%;
+    ${(p) =>
+      !p.isSelect &&
+      css`
+        transform: scale(0);
+      `}
+    background-color: ${(p) => (p.isSelect ? "#9995" : "#0000")};
+    transition: transform 0.3s cubic-bezier(0.65, 0.05, 0.1, 1);
+  }
+
+  &:hover {
+    cursor: pointer;
+    &::before {
+      content: " ";
+
+      z-index: 1;
+      background-color: #9997;
+      transform: scale(1);
+    }
+    &::after {
+      content: " ";
+      background-color: #9995;
+      transform: scale(1);
+    }
   }
 `;
 
@@ -35,8 +89,10 @@ const Tag = styled.div`
 `;
 
 const project = () => {
+  const [selectTagList, setSelectTagList] = useState<string[]>([]);
+
   const tagList = useMemo(() => {
-    let result: string[] = [];
+    let result: string[] = ["SHOW ALL"];
     ProjectDummy.forEach((v) =>
       v.tag.forEach((_forEachValue) => {
         if (result.includes(_forEachValue)) {
@@ -49,6 +105,17 @@ const project = () => {
     return result;
   }, []);
 
+  const onClickTag = useCallback(
+    (value: string) => {
+      if (selectTagList.includes(value)) {
+        setSelectTagList((prev) => prev.filter((v) => v !== value));
+      } else {
+        setSelectTagList((prev) => [...prev, value]);
+      }
+    },
+    [selectTagList]
+  );
+
   return (
     <Main>
       <Box>
@@ -56,7 +123,11 @@ const project = () => {
 
         <Tag>
           {tagList.map((v) => (
-            <TagItem className="tagItem">
+            <TagItem
+              onClick={() => onClickTag(v)}
+              isSelect={selectTagList.includes(v)}
+              className="tagItem"
+            >
               <p>{v}</p>
             </TagItem>
           ))}
